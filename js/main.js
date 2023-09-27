a8756b61a66f6747b01c3a6be5d48ea6ec3c6be7
Tricky71 <nowwhat@mail.ru>
2023-09-27 08:31:11 +0400

    footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+'use strict';
 
+document.addEventListener('DOMContentLoaded', function() {
+
+  const smoothLinks = document.querySelectorAll('a[href^="#links"]');
+  for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = document.getElementById('links');
+
+        id.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+  };
+});
 
 
 

6cab2941f62dc3f0e9c83fdd3135c93b4a3e0a5c
Tricky71 <nowwhat@mail.ru>
2023-10-04 19:40:42 +0400

    правки 4

--- js/main.js
+++ js/main.js
@@ -13,13 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
     
     smoothLinks[i].addEventListener('click', function (e) {
       e.preventDefault();
+      modal1.classList.add('hidden');
+      modal2.classList.add('hidden');
+      overlay.classList.add('hidden');
+      body1.style.overflowY = 'visible';
       smoothItems[arr[i] - 1].scrollIntoView({
         behavior: 'smooth',
         block: 'start'
       });
     });
   };
-  
+
+  const body1 = document.querySelector('body');
   const btn1 = document.querySelector('.button__active--1');
   const btn2 = document.querySelector('.button__active--2');
   const modal2= document.querySelector('.main-modal--2');
@@ -29,45 +34,54 @@ document.addEventListener('DOMContentLoaded', function() {
   const overlay = document.querySelector('.overlay');
   const backBtn1 = document.querySelector('.button-back--1');
   const backBtn2 = document.querySelector('.button-back--2');
+  const overlayInlay =document.querySelector('.overlay__inlay');
   
   btn1.addEventListener('click', () => {
     modal1.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
+    body1.style.overflowY = 'hidden';
   })
 
   btn2.addEventListener('click', () => {
     modal2.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
+    body1.style.overflowY = 'hidden';
   })
 
   closeBtn1.addEventListener('click', () => {
     modal1.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
+    body1.style.overflowY = 'visible';
   });
   
   closeBtn2.addEventListener('click', () => {
     modal2.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
-  })
+    body1.style.overflowY = 'visible';
+  });
 
   backBtn1.addEventListener('click', () => {
     modal1.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
-  })
+    body1.style.overflowY = 'visible';
+  });
 
   backBtn2.addEventListener('click', () => {
     modal2.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
-  })
+    body1.style.overflowY = 'visible';
+  });
 
   overlay.addEventListener('click', (evt) => {
-    let target = evt.target;
-    if (target != modal1 || target != modal2) {
+    if (evt.target == overlayInlay) {
       modal1.classList.add('hidden');
       modal2.classList.add('hidden');
       overlay.classList.add('hidden');
+      body1.style.overflowY = 'visible';
     }
-  })
+  });
+
+
 });
 
 
